7e6ec51b70d214dcc5fcfce4162599ade48e1e77
MineApp Admin <admin@mineapp.com>
2026-09-18 21:09:12 -0500

    Clarify modal labels to show both action and product name; Bump v228

--- js/app.js
+++ js/app.js
@@ -539,7 +539,7 @@ window.switchClient = function(client) {
         }
 
         if (label) {
-            label.textContent = product.name.toUpperCase();
+            label.textContent = "CANTIDAD: " + product.name.toUpperCase();
         }
         
         if (editIndex !== -1) {
@@ -566,7 +566,7 @@ window.switchClient = function(client) {
         }
 
         if (label) {
-            label.textContent = product.name.toUpperCase();
+            label.textContent = "PRECIO: " + product.name.toUpperCase();
         }
         
         if (editIndex !== -1) {
@@ -593,7 +593,7 @@ window.switchClient = function(client) {
         }
 
         if (label) {
-            label.textContent = product.name.toUpperCase();
+            label.textContent = "DETALLE: " + product.name.toUpperCase();
         }
         
         if (editIndex !== -1) {
